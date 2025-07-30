2ccfb87552623e97174c37757ab6f8933f0a459a
muhammadsodiq1429 <muhammadsodiqmuhammadjanov@gmail.com>
2025-07-30 14:45:33 +0500

    vercel.json file added to eliminate vercel deployment error. react-router-dom and react-icons packages downloaded

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Main from "./components/Main";
+import { BrowserRouter } from "react-router";
 
 const App = () => {
   return (
