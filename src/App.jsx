c1489361671a159620089d227f56bf31561e47fa
muhammadsodiq1429 <muhammadsodiqmuhammadjanov@gmail.com>
2025-07-31 06:18:39 +0500

    layout added

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,19 @@ import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Main from "./components/Main";
-import { BrowserRouter } from "react-router";
+import { BrowserRouter, Route, Routes } from "react-router";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Layout from "./pages/Layout/Layout";
 
 const App = () => {
   return (
-    <div>
-      <Header />
-      <Main />
-      <Footer />
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index={true} element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Route>
+    </Routes>
   );
 };
 
